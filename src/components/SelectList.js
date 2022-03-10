ddb56fc5cc9c1e20f4092db7d9d60258a306c20b
Andrew <zelenin.andrew@gmail.com>
2022-03-10 23:59:59 +0300

    added gaps in selected list

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import tw from 'twrnc';
 import {View, FlatList} from 'react-native';
 
+export const Gap = Symbol('Gap');
+
 export const SelectList = ({items, render}) => {
   return (
     <View style={tw`h-full`}>
@@ -11,12 +13,14 @@ export const SelectList = ({items, render}) => {
         renderItem={({item, index}) => (
           <View
             style={tw.style(
-              index === 0 && 'rounded-t-lg',
+              `border-white dark:border-black bg-gray-100 dark:bg-zinc-900 
+              min-h-12 justify-center px-3`,
               index !== items.length - 1 && 'border-b',
-              index === items.length - 1 && 'rounded-b-lg',
-              `border-white dark:border-black bg-gray-100 dark:bg-zinc-900 min-h-12 justify-center px-3`,
+              (index === 0 || items[index - 1] === Gap) && 'rounded-t-lg',
+              (index === items.length - 1 || items[index + 1] === Gap) && 'rounded-b-lg',
+              item === Gap && 'bg-white dark:bg-black h-3',
             )}>
-            {render(item)}
+            {item !== Gap ? render(item) : <View></View>}
           </View>
         )}
         keyExtractor={item => item.id}
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,13 +1,22 @@
 import React, {useCallback, useState} from 'react';
 import {View, Text, FlatList} from 'react-native';
-import {SelectList} from '../components/SelectList';
-import tw from "twrnc";
+import {SelectList, Gap} from '../components/SelectList';
+import tw from 'twrnc';
 
 export const SettingsScreen = () => {
   return (
     <SelectList
-      items={['Export CSV', 'Import CSV', 'Request Feature', 'Feedback']}
-      render={item => <Text style={tw`dark:text-zinc-200 text-base`}>{item}</Text>}
+      items={[
+        'Export to CSC file',
+        'Import',
+        Gap,
+        'Request Feature',
+        'Feedback',
+        'Contact Support',
+      ]}
+      render={item => (
+        <Text style={tw`dark:text-zinc-200 text-base`}>{item}</Text>
+      )}
     />
   );
 };
