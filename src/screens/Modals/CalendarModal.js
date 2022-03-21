776e2d0b66a6ea53af6ae19f24ae31c7c2c9f8b7
Andrew <zelenin.andrew@gmail.com>
2022-03-22 02:07:26 +0300

    today button on calendar modal

--- src/screens/Modals/CalendarModal.js
+++ src/screens/Modals/CalendarModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import tw from 'twrnc';
 import {useStoreActions, useStoreState} from 'easy-peasy';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -14,14 +14,23 @@ export const CalendarModal = ({navigation}) => {
   };
 
   return (
-    <View style={tw`flex h-full w-full items-center`}>
+    <View style={tw`flex h-full w-full items-center dark:bg-zinc-900`}>
       <DateTimePicker
         value={displayedDate}
         mode="date"
         display="inline"
-        style={tw`dark:bg-zinc-900 w-full h-full`}
+        style={tw`dark:bg-zinc-900 w-full `}
         onChange={onChange}
       />
+      <Pressable
+        style={tw`name:todayButton flex w-11/12 dark:bg-zinc-800
+             bg-gray-200 py-3 items-center rounded-lg`}
+        onPress={() => {
+          setDisplayedDate(new Date());
+          navigation.goBack();
+        }}>
+        <Text style={tw`text-blue-500 text-base`}>Today</Text>
+      </Pressable>
     </View>
   );
 };
